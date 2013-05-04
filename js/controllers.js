bd3af5c4daea676f77216363a0e0cee4526e0c1e
Radek <raduz74@gmail.com>
2013-05-04 18:00:47 +0200

    changing the list layout, add desc to bookmarks

--- js/controllers.js
+++ js/controllers.js
@@ -18,18 +18,18 @@ angular.module('BM.controllers', [])
   	var categoryId = $routeParams.categoryId;
   	var items = {
   		"angular": [
-  			{id:'b_1', link: 'http://angularjs.org/', title: "Angular JS project page", desc: ''},
-  			{id:'b_2', link: 'https://www.youtube.com/watch?v=M-MuOU3My-Y', title: "Vojta Jína přednáška", desc: ''},
-  			{id:'b_3', link: 'http://www.alexrothenberg.com/2013/02/11/the-magic-behind-angularjs-dependency-injection.html', title: "Dependency injection article" , desc: ''}
+  			{id:'b_1', link: 'http://angularjs.org/', title: "Angular JS project page", desc: 'the framework home page'},
+  			{id:'b_2', link: 'https://www.youtube.com/watch?v=M-MuOU3My-Y', title: "Vojta Jína přednáška", desc: 'GDG Spaghetti Code Liberec - Vojta Jína - AngularJS'},
+  			{id:'b_3', link: 'http://www.alexrothenberg.com/2013/02/11/the-magic-behind-angularjs-dependency-injection.html', title: "Dependency injection article" , desc: 'The "Magic" behind AngularJS Dependency Injection'}
   		],
   		"grunt": [
-  			{id:'b_4', link: 'http://gruntjs.com/', title: "Grunt home page", desc: ''},
-  			{id:'b_5', link: 'http://www.justinmccandless.com/blog/A+Tutorial+for+Getting+Started+with+Grunt', title: "getting started with grunt", desc: ''}
+  			{id:'b_4', link: 'http://gruntjs.com/', title: "Grunt home page", desc: 'Grunt home page'},
+  			{id:'b_5', link: 'http://www.justinmccandless.com/blog/A+Tutorial+for+Getting+Started+with+Grunt', title: "getting started with grunt", desc: 'quick and easy starting with grutn tutorial'}
   		],
   		"nodejs": [
-  			{id:'b_6', link: 'http://nodejs.org/', title: "Nodejs home page", desc: ''},
-  			{id:'b_7', link: 'http://nodejs.org/', title: "Nodejs home page", desc: ''},
-  			{id:'b_8', link: 'http://nodejs.org/', title: "Nodejs home page", desc: ''}
+  			{id:'b_6', link: 'http://nodejs.org/', title: "Nodejs home page", desc: 'some desc'},
+  			{id:'b_7', link: 'http://nodejs.org/', title: "Nodejs home page", desc: 'other desc'},
+  			{id:'b_8', link: 'http://nodejs.org/', title: "Nodejs home page", desc: 'yet another desc'}
   		]
   	}
   	$scope.title = categoryId;
