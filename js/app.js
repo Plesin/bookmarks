3f25ffc73a74e26e753e61048f4e73ec6b71a97f
Radek <raduz74@gmail.com>
2013-05-11 15:31:06 +0200

    data to factory, add date to note

--- js/app.js
+++ js/app.js
@@ -1,8 +1,38 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('BM', ['BM.controllers']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/:tagId', {templateUrl: 'partials/note.html', controller: 'NotesCtrl'});
-    $routeProvider.otherwise({redirectTo: '/'});
-  }]);
+var App = angular.module('App', []);
+
+App.factory('Tags', function() {
+    var tags = [
+        {id: 'angularjs', title: "Angular JS"},
+        {id: 'grunt', title: "Grunt"},
+        {id:'nodejs', title: "Node JS"}
+    ];
+    return tags;
+});
+
+App.factory('NotesData', function() {
+    var Notes =
+    [
+        {time: 1362694426773, title: "Angular JS project page", content: 'http://angularjs.org/', tags: ['angular']},
+        {time: 1362693145553, title: "Vojta Jína přednáška", content: 'https://www.youtube.com/watch?v=M-MuOU3My-Y' ,tags: ['angular']},
+        {time: 1360620005000, title: "Dependency injection article" , content: 'http://www.alexrothenberg.com/2013/02/11/the-magic-behind-angularjs-dependency-injection.html', tags: ['angular']},
+        {time: 1360676013000, title: "Grunt home page", content: 'http://gruntjs.com/', tags: ['grunt']},
+        {time: 1360785600000, title: "getting started with grunt", content: 'http://www.justinmccandless.com/blog/A+Tutorial+for+Getting+Started+with+Grunt', tags: ['grunt']},
+        {time: 1360736155000, title: "Nodejs home page", content: 'http://nodejs.org/', tags: ['grunt']},
+        {time: 1360832121000, title: "Nodejs home page", content: 'http://nodejs.org/', tags: ['grunt']},
+        {time: 1360944318917, title: "Nodejs home page", content: 'http://nodejs.org/', tags: ['grunt']}
+
+    ];
+    return Notes;
+});
+
+App.directive('inputEvents', function(){
+    return function(scope, element) {
+        element.unbind('focus').bind('focus', function() {
+            var elems = element.parent().children();
+            elems.removeClass('invisible');
+        });
+    };
+});
--- js/controllers.js
+++ js/controllers.js
@@ -1,29 +1,13 @@
 'use strict';
 
-angular.module('BM.controllers', [])
-    .controller('NotesCtrl', ['$scope', function($scope) {
-      	$scope.tags = [
-        	{id: 'angularjs', title: "Angular JS"},
-        	{id: 'grunt', title: "Grunt"},
-        	{id:'nodejs', title: "Node JS"}
-    	];
+function NotesCtrl($scope, Tags, NotesData) {
+    $scope.tags = Tags;
+    $scope.notes = NotesData;
 
-        $scope.notes =
-        [
-            {id:'n_1', title: "Angular JS project page", content: 'http://angularjs.org/', tags: ['angular']},
-            {id:'n_2', title: "Vojta Jína přednáška", content: 'https://www.youtube.com/watch?v=M-MuOU3My-Y' ,tags: ['angular']},
-            {id:'n_3', title: "Dependency injection article" , content: 'http://www.alexrothenberg.com/2013/02/11/the-magic-behind-angularjs-dependency-injection.html', tags: ['angular']},
-            {id:'n_4', title: "Grunt home page", content: 'http://gruntjs.com/', tags: ['grunt']},
-            {id:'n_5', title: "getting started with grunt", content: 'http://www.justinmccandless.com/blog/A+Tutorial+for+Getting+Started+with+Grunt', tags: ['grunt']},
-            {id:'n_6', title: "Nodejs home page", content: 'http://nodejs.org/', tags: ['grunt']},
-            {id:'n_7', title: "Nodejs home page", content: 'http://nodejs.org/', tags: ['grunt']},
-            {id:'n_8', title: "Nodejs home page", content: 'http://nodejs.org/', tags: ['grunt']}
-        ];
-
-    	$scope.addNote = function() {
-    		var id = $scope.title.toLowerCase().replace(/\s+/g, '');
-    		$scope.notes.push({id: id, title: $scope.title, content: $scope.content});
-    		$scope.title = '';
-            $scope.content = '';
-    	};
-    }]);
+    $scope.addNote = function() {
+        var id = $scope.title.toLowerCase().replace(/\s+/g, '');
+        $scope.notes.push({time: (new Date().getTime()), title: $scope.title, content: $scope.content});
+        $scope.title = '';
+        $scope.content = '';
+    };
+}
