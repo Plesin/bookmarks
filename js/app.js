3e376bcc95a6562f3911915d3182b85b0d4c3c85
Radek - Win7 <radek.pleskac@hotmail.com>
2013-05-27 15:32:41 +0200

    adding tags, valid json, testing SourceTree from Win7

--- js/app.js
+++ js/app.js
@@ -15,14 +15,14 @@ App.factory('Tags', function() {
 App.factory('NotesData', function() {
     var Notes =
     [
-        {time: 1362694426773, title: "Angular JS project page", content: 'http://angularjs.org/', tags: ['angular']},
-        {time: 1362693145553, title: "Vojta Jína přednáška", content: 'https://www.youtube.com/watch?v=M-MuOU3My-Y' ,tags: ['angular']},
-        {time: 1360620005000, title: "Dependency injection article" , content: 'http://www.alexrothenberg.com/2013/02/11/the-magic-behind-angularjs-dependency-injection.html', tags: ['angular']},
-        {time: 1360676013000, title: "Grunt home page", content: 'http://gruntjs.com/', tags: ['grunt']},
-        {time: 1360785600000, title: "getting started with grunt", content: 'http://www.justinmccandless.com/blog/A+Tutorial+for+Getting+Started+with+Grunt', tags: ['grunt']},
-        {time: 1360736155000, title: "Nodejs home page", content: 'http://nodejs.org/', tags: ['grunt']},
-        {time: 1360832121000, title: "Nodejs home page", content: 'http://nodejs.org/', tags: ['grunt']},
-        {time: 1360944318917, title: "Nodejs home page", content: 'http://nodejs.org/', tags: ['grunt']}
+        {"time": "1362694426773", "title": "Angular JS project page", "content": "http://angularjs.org/", "tags": ["angularjs"]},
+        {"time": "1362693145553", "title": "Vojta Jína přednáška", "content": "https://www.youtube.com/watch?v=M-MuOU3My-Y" ,"tags": ["angularjs"]},
+        {"time": "1360620005000", "title": "Dependency injection article" , "content": "http://www.alexrothenberg.com/2013/02/11/the-magic-behind-angularjs-dependency-injection.html", "tags": ["angularjs"]},
+        {"time": "1360676013000", "title": "Grunt home page", "content": "http://gruntjs.com/", "tags": ["grunt"]},
+        {"time": "1360785600000", "title": "getting started with grunt", "content": "http://www.justinmccandless.com/blog/A+Tutorial+for+Getting+Started+with+Grunt", "tags": ["grunt"]},
+        {"time": "1360736155000", "title": "Nodejs home page", "content": "http://nodejs.org/", "tags": ["nodejs"]},
+        {"time": "1360832121000", "title": "Nodejs home page", "content": "http://nodejs.org/", "tags": ["nodejs"]},
+        {"time": "1360944318917", "title": "Nodejs home page", "content": "http://nodejs.org/", "tags": ["nodejs"]}
 
     ];
     return Notes;
--- js/controllers.js
+++ js/controllers.js
@@ -3,11 +3,19 @@
 function NotesCtrl($scope, Tags, NotesData) {
     $scope.tags = Tags;
     $scope.notes = NotesData;
+    $scope.newNoteTags = [];
+    $scope.filterTags = ['angularjs'];
+    $scope.selectedTag = '';
 
     $scope.addNote = function() {
         var id = $scope.title.toLowerCase().replace(/\s+/g, '');
-        $scope.notes.push({time: (new Date().getTime()), title: $scope.title, content: $scope.content});
+        $scope.notes.push({time: (new Date().getTime()), title: $scope.title, content: $scope.content, tags: $scope.newNoteTags});
         $scope.title = '';
         $scope.content = '';
+        $scope.newNoteTags = [];
+    };
+
+    $scope.selectTag = function(id) {
+        $scope.selectedTag = id;
     };
 }
